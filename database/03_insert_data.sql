199913cc1068de70215a620a83c07ac5539b26ee
SunnyTany <46300377+SunnyTany@users.noreply.github.com>
2026-09-03 14:20:41 +0200

    refactor insert

--- database/03_insert_data.sql
+++ database/03_insert_data.sql
@@ -1,4 +1,4 @@
-SINSERT INTO users (name, age, email) VALUES
+INSERT INTO users (name, age, email) VALUES
 ('John', 30, 'john@example.com'),
 ('Alice', 25, 'alice@example.com'),
 ('Bob', 35, 'bob@example.com');
