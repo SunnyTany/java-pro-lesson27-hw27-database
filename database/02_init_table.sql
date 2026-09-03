8a8fefd51b3ac69315d2079048eb8b1e1ed90ce9
SunnyTany <46300377+SunnyTany@users.noreply.github.com>
2026-09-03 14:10:28 +0200

    delete images refactor create table

--- database/02_init_table.sql
+++ database/02_init_table.sql
@@ -1,11 +1,11 @@
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY, -- В PostgreSQL вместо AUTO_INCREMENT используется тип SERIAL
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    age INT,
-    email VARCHAR(255)
+    age INT CHECK (age >= 0),
+    email VARCHAR(255) NOT NULL UNIQUE
 );
 
-INSERT INTO users (name, age, email) VALUES 
+INSERT INTO users (name, age, email) VALUES
 ('John', 30, 'john@example.com'),
 ('Alice', 25, 'alice@example.com'),
 ('Bob', 35, 'bob@example.com');
